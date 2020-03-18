dbbc4cd25c7fe5622651a8e88e692c4bd0cbf643
martinmt-mx <mmojica0@ucol.mx>
2020-03-18 01:42:54 -0600

    tiempo-encap

--- tiempo.js
+++ tiempo.js
@@ -6,31 +6,31 @@ export default class Tiempo{
      * @param {string} periodo 
      */
     constructor(hora, minutos, periodo = "am"){
-        this.hora = hora
-        this.minutos = minutos
-        this.periodo = periodo.toLowerCase()
+        this._hora = hora
+        this._minutos = minutos
+        this._periodo = periodo.toLowerCase()
     }
     getFormato12(){
-        if (this.periodo === "am" && this.hora==12){
-            return `${this.hora - 12}:${this.minutos} ${this.periodo}`
+        if (this._periodo === "am" && this._hora==12){
+            return `${this._hora - 12}:${this._minutos} ${this._periodo}`
         }
-        else if (this.periodo === "pm" && this.hora>12){
-            return `${this.hora - 12}:${this.minutos} ${this.periodo}`
+        else if (this._periodo === "pm" && this._hora>12){
+            return `${this._hora - 12}:${this._minutos} ${this._periodo}`
         }
         else{
-            return `${this.hora}:${this.minutos} ${this.periodo}`
+            return `${this._hora}:${this._minutos} ${this._periodo}`
         }
     }
     getFormato24(){
-        let hora24 = this.hora
-        if (this.periodo === "am" && this.hora==12){
-            return `${this.hora - 12}:${this.minutos} ${this.periodo}`
+        this._hora24 = this._hora
+        if (this._periodo === "am" && this._hora==12){
+            return `${this._hora - 12}:${this._minutos} ${this._periodo}`
         }
-        else if (this.periodo === "pm" && this.hora < 12){
-            return `${hora24+12}:${this.minutos} ${this.periodo}`
+        else if (this._periodo === "pm" && this._hora < 12){
+            return `${this._hora24+12}:${this._minutos} ${this._periodo}`
         }
         else{
-            return `${this.hora}:${this.minutos} ${this.periodo}`
+            return `${this._hora}:${this._minutos} ${this._periodo}`
         }
         
     }
